51f018f6bc9ff3f05b23eacf6da17f47942f4a52
atlasfoo@lin <jf_mejiar547@live.com>
2020-06-16 11:20:58 -0600

    employees

---
+++ appexe/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TarjetaFruta from './components/TarjetaFruta';
+
+
+const App =()=>(
+	<div>
+    <TarjetaFruta name={'Sandia'} price={5.00} />
+    <TarjetaFruta name={'Naranja'} price={1.50} />
+    <TarjetaFruta name='Kiwi' price={3.30} />
+  </div>
+);
+
+export default App;
---
+++ appexe/src/components/TarjetaFruta.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+class TarjetaFruta extends React.Component{
+	state={
+		cantidad: 0
+	}
+
+	agregar = ()=>{
+		this.setState({'cantidad': this.state.cantidad +1})
+	}
+
+	quitar = ()=>{
+		this.setState({'cantidad': this.state.cantidad -1})
+	}
+	limpiar = () =>{
+		this.setState({'cantidad': 0})
+	}
+	
+
+	render (){
+		return (
+			<div>
+				<h3>{ this.props.name}</h3>
+				<p>Cantidad: {this.state.cantidad}</p>
+				<button onClick={this.agregar}>+</button>
+				<button onClick={this.quitar}>-</button>
+				<button onClick={this.limpiar}>Limpiar</button>
+				<hr/>
+				<p>$ {this.props.price}</p>
+			</div>
+		)
+	}
+}
+
+export default TarjetaFruta;
